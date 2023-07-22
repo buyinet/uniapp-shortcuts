c55115edf2ebacf9b2233760bc3a196ee8a91df8
自由的道德群众 <2453201633@qq.com>
2023-07-23 07:25:50 +0800

    update

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -10,41 +10,9 @@
 			}
 		},
 		onLoad() {
-			this.checkArguments(); // 检测启动参数 // 这是默认的监听参数 也就是应用初始化的时候监听  
-
-			// 监听后台恢复 这是利用5+的方式 处理 APP进入后台后 再进入到APP前台时参数监听  
-			plus.globalEvent.addEventListener('newintent', (e) => {
-				this.checkArguments(); // 检测启动参数  
-			});
+			plus.runtime.openURL("shortcuts://run-shortcut?name=给快捷指令传递参数实例&input=https://www.baidu.com");
 		},
 		methods: {
-			checkArguments() {
-				console.log('Shortcut-plus.runtime.launcher: ' + plus.runtime.launcher);
-				if (plus.runtime.launcher == 'shortcut') {
-					// 通过快捷方式启动，iOS平台表示通过3D Touch快捷方式，Android平台表示通过桌面快捷方式启动  
-					try {
-						var cmd = JSON.parse(plus.runtime.arguments);
-						console.log('Shortcut-plus.runtime.arguments: ' + plus.runtime.arguments);
-						var type = cmd && cmd.type;
-						// 可以自行根据type 处理 你的业务逻辑  
-
-						setTimeout(r => {
-							switch (type) {
-								case 'scan':
-									uni.showToast({
-										title:"test"
-									})
-									
-									break;
-							}
-						}, 800);
-
-						console.log(JSON.stringify(cmd));
-					} catch (e) {
-						console.log('Shortcut-exception: ' + e);
-					}
-				}
-			},
 		}
 	}
 </script>
