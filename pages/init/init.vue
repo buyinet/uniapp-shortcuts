683448e19e31dfbed75abfb74acc5a30b6edf719
自由的道德群众 <2453201633@qq.com>
2023-07-23 11:56:07 +0800

    update

--- pages/init/init.vue
+++ pages/init/init.vue
@@ -1,13 +1,37 @@
 <template>
-  <view>
-  </view>
+	<view>
+    <view
+    :style="'height:'+statusBarHeight+'px'"
+    ></view>
+    <view
+    style="height: 48px;"
+    ></view>
+		<view class="box">
+			<kt-button>下载快捷捷径</kt-button>
+      <view style="height: 20rpx;"></view>
+			<kt-button>下载快捷捷径</kt-button>
+      <view style="height: 20rpx;"></view>
+			<kt-button>下载快捷捷径</kt-button>
+		</view>
+	</view>
 </template>
 
 <script>
-export default {
-}
+	export default {
+    data(){
+      return {
+        statusBarHeight: 0,
+      }
+    },
+    created(){
+      this.statusBarHeight = uni.getSystemInfoSync().statusBarHeight;
+    }
+  }
 </script>
 
-<style lang="scss">
-
+<style lang="scss" scoped>
+.box{
+  padding: 30rpx;
+  box-sizing: border-box;
+}
 </style>
